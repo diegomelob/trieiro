c255a44e5a80775828c01518d9fbc605649feb4c
Diego Melo <d_mello2010@outlook.com>
2020-06-13 22:04:34 -0300

    feat: Inclusao dependencia MongoDb

--- api-ccr-hackathon/v1/controllers/index.js
+++ api-ccr-hackathon/v1/controllers/index.js
@@ -3,10 +3,12 @@
 const factory = require('./factory');
 const config = require('../../config');
 const services = require('../../services');
+const mongoDb = require('../../commons/mongoDb');
 
 const adapters = require('../adapters')({
   config,
   services,
+  mongoDb,
 });
 
 module.exports = factory(adapters);
