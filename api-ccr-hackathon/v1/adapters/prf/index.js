03f20ed361a866c3bc89177a8b33c20678a6c2f3
Talles Chaves <k2.talles@grupofleury.com.br>
2020-06-14 18:36:00 -0300

    Ajustes de coordenadas lat/lng, inserção de distanciamento e ajuste de filtros de pesquisa no mongo para o endpoint pontos

--- api-ccr-hackathon/v1/adapters/prf/index.js
+++ api-ccr-hackathon/v1/adapters/prf/index.js
@@ -1,17 +1,18 @@
 'use strict';
+const distanceCoordinates = require('../../../commons/utils/distanceCoordinates');
 
 const mapRequest = (payload) => {
   let response = {};
-  if(payload.uf) {
-    response.uf = payload.uf;
+  if (payload.uf) {
+    response.sg_uf = payload.uf.toUpperCase();;
   }
-  if(payload.municipio) {
+  if (payload.municipio) {
     response.municipio = payload.municipio;
   }
-  if(payload.categoria) {
+  if (payload.categoria) {
     response.categoria = payload.categoria;
   }
-  if(payload.rodovia) {
+  if (payload.rodovia) {
     response.br = payload.rodovia;
   }
   return response;
@@ -24,12 +25,12 @@ const prfWrapper = ({
   const prf = async ({
     payload,
   }) => {
-    try {    
+    try {
       let lat = parseFloat(payload.latitude);
       let lng = parseFloat(payload.longitude);
 
       let query = {
-        ...mapRequest(payload),
+        ...mapRequest,
         point: {
           $near: {
             $geometry: {
@@ -40,8 +41,17 @@ const prfWrapper = ({
           }
         }
       };
-
+      console.log(JSON.stringify(query));
       const response = await mongoDb.search(config.db.mongoDB, mongoDb, 'pontos', query);
+      response.map(async ponto => {
+        ponto.rating = Math.floor(Math.random() * 5);
+        ponto.latitude = ponto.point.coordinates[0];
+        ponto.longitude = ponto.point.coordinates[1];
+        ponto.distance = await distanceCoordinates.calculate(lat, lng, ponto.latitude, ponto.longitude, 'M');
+
+        delete ponto.point;
+        delete ponto._id;
+      })
       return response;
     } catch (errorCatch) {
       throw errorCatch;
